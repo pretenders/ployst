e2425ccd4b65c4e6cd9a91e54ba96ff71095c13f
Carles Barrobés <carles@barrobes.com>
2014-02-01 16:13:13 +0100

    Add permissions and dynamic fields mixins to features

--- ployst/core/features/serializers.py
+++ ployst/core/features/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 
+from ployst.apibase.mixins import DynamicFieldsSerializerMixin
 from .models import Feature
 
 
-class FeatureSerializer(serializers.ModelSerializer):
+class FeatureSerializer(DynamicFieldsSerializerMixin,
+                        serializers.ModelSerializer):
     class Meta:
         model = Feature
+        fields = ('id', 'provider', 'feature_id', 'type', 'title')
