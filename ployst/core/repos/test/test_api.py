5e2e322a74050b75d03893b2360481081cf98878
Carles Barrobés <carles@barrobes.com>
2014-02-01 23:30:25 +0100

    Fix tests that failed due to ownership checks

--- ployst/core/repos/test/test_api.py
+++ ployst/core/repos/test/test_api.py
@@ -3,29 +3,41 @@ import json
 from django.core.urlresolvers import reverse
 from rest_framework.test import APITestCase
 
+from ployst.core.accounts.test.mixins import ProjectTestMixin
+
 from .factories import RepositoryFactory
 
 
-class TestFiltering(APITestCase):
+class TestFiltering(ProjectTestMixin, APITestCase):
 
     def test_get_repos_by_url(self):
         """
-        Test we search by url to get a list of repos.
+        Search by url to get a list of matching repos.
 
         """
         repo_url = 'http://github.com/pretenders/ployst'
-        repo = RepositoryFactory(name='PloystTest', url=repo_url)
-
+        repo1 = RepositoryFactory(
+            name='PloystTest', url=repo_url, project=self.project)
+        RepositoryFactory(name='PloystTest', project=self.project)
         url = reverse('core:repos:repository-list')
+
         response = self.client.get('{0}?url={1}'.format(url, repo_url))
 
         repos = json.loads(response.content)
         self.assertEquals(len(repos), 1)
-        self.assertEquals(repos[0]['name'], repo.name)
+        self.assertEquals(repos[0]['name'], repo1.name)
 
     def test_only_see_repos_for_my_team(self):
-        "Should not be able to see all repos."
-        # How do we want to handle security in the API.
-        # Should all requests be sent with some kind of team token?
-        # Should the backend (ie providers) also need to use such a token?
-        raise NotImplementedError("test_only_see_repos_for_my_team")
+        """
+        Should not be able to see all repos.
+
+        """
+        repo1 = RepositoryFactory(name='TestRepo-1', project=self.project)
+        RepositoryFactory(name='TestRepo-2')
+        url = reverse('core:repos:repository-list')
+
+        response = self.client.get(url)
+
+        repos = json.loads(response.content)
+        self.assertEquals(len(repos), 1)
+        self.assertEquals(repos[0]['name'], repo1.name)
