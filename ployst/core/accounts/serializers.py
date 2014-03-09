fe7774615a3cd5a42a9e6257e042d5818d720f7b
Carles Barrobés <carles@barrobes.com>
2014-03-09 23:28:54 +0100

    Add a view at /accounts/me for details of logged in user

--- ployst/core/accounts/serializers.py
+++ ployst/core/accounts/serializers.py
@@ -1,6 +1,20 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 from .models import Project, ProjectManager
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """
+    User data for a profile and logged-in page.
+
+    """
+    class Meta:
+        model = User
+        depth = 1
+        fields = (
+            'id', 'username', 'first_name', 'last_name', 'email',
+        )
 
 
 class ProjectManagerSerializer(serializers.ModelSerializer):
--- ployst/core/accounts/views.py
+++ ployst/core/accounts/views.py
@@ -1,7 +1,27 @@
+from rest_framework.generics import RetrieveAPIView
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from .models import Project, Team, ProjectProviderSettings
-from .serializers import ProjectSerializer
+from .serializers import ProjectSerializer, UserSerializer
+
+
+class MyAccountView(RetrieveAPIView):
+    """
+    API to retrieve information about the logged in user.
+
+    To be used in a profile page.
+
+    """
+    serializer_class = UserSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        """
+        Serialize current logged-in user.
+
+        """
+        serializer = self.get_serializer(request.user)
+        return Response(serializer.data)
 
 
 class ProjectViewSet(ModelViewSet):
