1d0b971f2874d96ab95687e6cfe0a6d065b5b942
Carles Barrobés <carles@barrobes.com>
2014-03-25 22:45:45 +0100

    Use tox to build the project

--- ployst/settings/test.py
+++ ployst/settings/test.py
@@ -2,3 +2,18 @@ from .dev import *  # NOQA
 
 CELERY_ALWAYS_EAGER = True
 CELERY_EAGER_PROPAGATES_EXCEPTIONS = True
+
+
+# not so verbose factory boy output
+LOGGING['loggers'].update({
+    'django.db': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+        'propagate': True,
+    },
+    'factory': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+        'propagate': True,
+    },
+})
