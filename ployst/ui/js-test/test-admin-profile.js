1c6014cbdeb970dbd49766e0ea8ba54fc61599f0
Carles Barrobés <carles@barrobes.com>
2014-04-08 14:27:30 +0200

    Angular tests: karma setup and tests for profile

---
+++ ployst/ui/js-test/test-admin-profile.js
@@ -0,0 +1,25 @@
+describe('test profile scope', function () {
+
+    var ctrl, scope, mockUser;
+    mockUser = {username: 'my-name', email: 'me@ployst.com'};
+
+    beforeEach(module('ployst'));
+
+    beforeEach(inject(function($rootScope, $route, User, $controller) {
+        User.user = mockUser;
+        // mock routing
+        $route.current = {'$$route': {menu: 'profile'}};
+
+        scope = $rootScope.$new();
+        ctrl = $controller(
+            ployst.ng.controllers.profile,
+            {$scope: scope}
+        );
+    }));
+
+    it('should include user with username and email', function () {
+        expect(scope.user.username).toBe('my-name');
+        expect(scope.user.email).toBe('me@ployst.com');
+    });
+
+});
