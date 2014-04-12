29324db2db88948b9be06ff2ab6aa051daa26fad
Carles Barrobés <carles@barrobes.com>
2014-04-12 23:02:02 +0200

    Dependency management simplified, no more global npm installs

--- fabfile.py
+++ fabfile.py
@@ -1,29 +1,9 @@
 from os import environ
-from os.path import dirname, exists, join
+from os.path import dirname, exists
 from fabric.api import abort, local, task
-from fabric.contrib.console import confirm
 
 
 THIS_DIR = dirname(__file__)
-
-
-def install_from_file(command, filename, version_separator,
-                      command_opts=''):
-    """
-    Install requirement from a file with the given command.
-
-    The files use the standard version separator '==' which will get
-    replaced with whatever you pass in here in `version_separator`.
-
-    """
-    dependencies = join(THIS_DIR, 'requirements', filename + '.txt')
-    with open(dependencies, 'r') as f:
-        for dependency in f.readlines():
-            if not dependency.strip().startswith('#'):
-                dependency = dependency.split('#')[0].strip()
-                dependency = version_separator.join(dependency.split('=='))
-                cmd = '{0} {1} {2}'.format(command, dependency, command_opts)
-                local(cmd)
 
 
 @task
@@ -32,24 +12,13 @@ def develop():
     Update environment for local development
 
     """
-    install_npm_mods = False
-    local('pip install -r requirements/dev.txt')
     venv = environ.get('VIRTUAL_ENV')
-    npm = local('which npm', capture=True)
 
     if not venv:
         abort('You must be in a virtual environment')
 
-    if venv not in npm:
-        if confirm('Install npm in your virtual env {0}?'.format(venv)):
-            local('nodeenv -p')
-            install_npm_mods = True
-    else:
-        install_npm_mods = True
-
-    if install_npm_mods:
-        install_from_file('npm install -g', 'npm-modules', '@')
-        local('bower install')
+    local('pip install -r requirements/dev.txt')
+    local('npm install')
 
 
 @task
@@ -59,8 +28,7 @@ def heroku_package_npm():
     """
     if not exists('package.json'):
         local('npm init')
-    install_from_file('npm install', 'npm-modules', '@',
-                      command_opts='--save')
+    local('npm install')
 
 
 @task
