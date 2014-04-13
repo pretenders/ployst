721256744d1eb18763366f5678cad177953e6a2a
Alex Couper <amcouper@gmail.com>
2014-04-13 19:10:22 -0400

    Make git hook checks configurable.

--- pre_commit.py
+++ pre_commit.py
@@ -7,8 +7,8 @@ you would like to run.
 
 To add a new check:
 
-    1. Create a function that returns True for success and False
-       for failure.
+    1. Create a function that returns a true like, printable object for
+       failure, or ``None`` for success.
     2. Add that function to ``CHECKS``.
     3. Add a corresponding line (the name of the function) to your tox.ini file
        with 'on' (run this check) or 'off' (don't). The default behaviour is to
@@ -21,7 +21,7 @@ from subprocess import Popen, PIPE
 
 
 class bash(object):
-    "This is lower class because it is intended to be used as a method"
+    "This is lower class because it is intended to be used as a method."
 
     def __init__(self, cmd):
         """
@@ -44,6 +44,7 @@ class bash(object):
 
 
 def title_print(msg):
+    "Pretty print a title bar."
     bar = '=' * 79
     print bar
     print msg
@@ -51,6 +52,7 @@ def title_print(msg):
 
 
 def python_files_for_commit():
+    "Get all python files that are staged for commit."
     files_pattern = '\.py(\..+)?$'
     return bash((
         "git diff --cached --name-only | "
@@ -58,32 +60,27 @@ def python_files_for_commit():
     ).format(files_pattern=files_pattern))
 
 
-def no_pdb():
+def pdb():
     "Look for pdb.set_trace() commands in python files."
     forbidden = '^[^#"]*pdb.set_trace()'
     py_files = python_files_for_commit()
     if not py_files:
-        return True
+        return
     files = py_files.bash((
         "xargs grep --color --with-filename -n "
         "-e '{forbidden}'"
     ).format(
         forbidden=forbidden
     ))
-    if files:
-        print "pdb", files
-    return not files
+    return files
 
 
-def no_flake8():
-    "Should be no flake8 errors"
-    files = str(python_files_for_commit())
-    if not files:
-        return True
-    errors = bash("flake8 {0}".format(files.replace('\n', ' ')))
-    if errors:
-        print "errors", errors
-    return not errors
+def flake8():
+    "Check flake8 errors in the code base."
+    py_files = str(python_files_for_commit())
+    if not py_files:
+        return
+    return bash("flake8 {0}".format(py_files.replace('\n', ' ')))
 
 
 @contextmanager
@@ -101,19 +98,19 @@ def gitstash():
         bash("git stash pop -q")
 
 
-def get_config():
+def get_hook_checks():
     """
-    Return the configuration options set in tox.ini
+    Return the hook check options set in tox.ini.
 
     Return an empty dict if none are set.
     """
     config = ConfigParser.ConfigParser()
     config.readfp(open('tox.ini'))
-    if config.has_section('githooks'):
-        return {key: value for key, value in config.items('githooks')}
+    if config.has_section('hook-checks'):
+        return {key: value for key, value in config.items('hook-checks')}
     return {}
 
-CHECKS = (no_pdb, no_flake8)
+CHECKS = (pdb, flake8)
 
 
 def main():
@@ -125,13 +122,15 @@ def main():
         0 - accept commit
     """
     exit_code = 0
-    conf_githooks = get_config()
+    hook_checks = get_hook_checks()
     with gitstash():
         for func in CHECKS:
             name = func.__name__
-            if conf_githooks.get(name, 'on') == 'on':
-                title_print("Checking {0}".format(name))
-                if not func():
+            if hook_checks.get(name, 'on') == 'on':
+                errors = func()
+                if errors:
+                    title_print("Checking {0}".format(name))
+                    print(errors)
                     exit_code = 1
     if exit_code == 1:
         title_print("Rejecting commit")
