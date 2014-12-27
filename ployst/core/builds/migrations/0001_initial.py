9d3c585367f693da4003fb71e0fe523f6efad6fa
Carles Barrobés <carles@barrobes.com>
2014-12-27 23:31:04 +0100

    Update to django 1.7

--- ployst/core/builds/migrations/0001_initial.py
+++ ployst/core/builds/migrations/0001_initial.py
@@ -1,42 +1,29 @@
 # -*- coding: utf-8 -*-
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import ployst.core.repos.models
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'Build'
-        db.create_table(u'builds_build', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('provider', self.gf('django.db.models.fields.CharField')(max_length=40)),
-            ('job_id', self.gf('django.db.models.fields.CharField')(max_length=100)),
-            ('revision', self.gf('ployst.core.repos.models.Revision')(max_length=40)),
-            ('status', self.gf('django.db.models.fields.CharField')(default='gray', max_length=6)),
-            ('url', self.gf('django.db.models.fields.URLField')(max_length=200)),
-            ('build_time', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
-        ))
-        db.send_create_signal(u'builds', ['Build'])
+    dependencies = [
+    ]
 
-
-    def backwards(self, orm):
-        # Deleting model 'Build'
-        db.delete_table(u'builds_build')
-
-
-    models = {
-        u'builds.build': {
-            'Meta': {'object_name': 'Build'},
-            'build_time': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'job_id': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'provider': ('django.db.models.fields.CharField', [], {'max_length': '40'}),
-            'revision': ('ployst.core.repos.models.Revision', [], {'max_length': '40'}),
-            'status': ('django.db.models.fields.CharField', [], {'default': "'gray'", 'max_length': '6'}),
-            'url': ('django.db.models.fields.URLField', [], {'max_length': '200'})
-        }
-    }
-
-    complete_apps = ['builds']
+    operations = [
+        migrations.CreateModel(
+            name='Build',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('provider', models.CharField(help_text=b'The CI provider that created this build', max_length=40)),
+                ('job_id', models.CharField(help_text=b'An identifier filled in by the provider', max_length=100)),
+                ('revision', ployst.core.repos.models.Revision(max_length=40)),
+                ('status', models.CharField(default=b'gray', max_length=6, choices=[(b'gray', b'not built'), (b'red', b'failure'), (b'yellow', b'unstable'), (b'green', b'success')])),
+                ('url', models.URLField()),
+                ('build_time', models.DateTimeField(auto_now_add=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
